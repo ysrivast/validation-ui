3cd25773c159852bd39e332780ca1830412bf738
yogeshsrivastava <writetoyoges@gmail.com>
2024-09-24 16:02:32 +0530

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FileRoutingModule } from './file/file-routing.module';
+import { HomeModule } from './home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'file', loadChildren: ()=>FileRoutingModule},
+  {path:'', loadChildren: ()=>HomeModule}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FileModule } from './file/file.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FileModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
